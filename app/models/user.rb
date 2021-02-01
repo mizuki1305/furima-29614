e719fd6f0049639ada7d8ed91dd880fae94f09fc
Hiroki Seike <sakaki.mizuki.13059029@gmail.com>
2021-02-01 21:27:16 +0900

    ログイン、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,15 @@ class User < ApplicationRecord
   
   has_many :listings
   has_many :purchases
-  
+
+  with_options presence: true do
+    validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥々]/}
+    validates :email, format: { with: /\A[a-zA-Z0-9]+\z/}
+    validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/, greater_than_or_equal_to: 6}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/}
+    validates :family_name_ruby, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_ruby, format: { with: /\A[ァ-ヶー－]+\z/}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'items', to: 'items#index'
   resources :items, only: :index
   root 'items#index'
 end
