1ac9d03a71934f1c9d811406acdee00a613a35e8
Hiroki Seike <sakaki.mizuki.13059029@gmail.com>
2021-02-03 12:35:10 +0900

    ユーザー管理機能の実装（修正4）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,15 +11,6 @@ describe User do
     context '新規登録がうまくいくとき' do
       it "nickname,email,password,password_confirmation,family_name,first_name,family_name_ruby,first_name_ruby,birthdayが存在すれば登録できる" do
         expect(@user).to be_valid
-        # binding.pry
-      end
-      it "passwordが6文字以上であれば登録できる" do
-        @user.password = "000000"
-        @user.password_confirmation = "000000"
-        expect(@user).to be_valid
-      end
-      it "emailに@が存在しないと登録できない"do
-        expect(@user).to be_valid
       end
     end
 
