1ea0195b5451230804687d5539e26750150d83c1
Hiroki Seike <sakaki.mizuki.13059029@gmail.com>
2021-02-03 15:21:33 +0900

    ユーザー管理機能の実装（修正6）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :family_name, :first_name, :family_name_ruby, :first_name_ruby, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_ruby, :first_name_ruby, :birthday])
   end
 
   private
