0fb2174dbaf2034716d865ffa13ef6bbc8c99de1
Hiroki Seike <sakaki.mizuki.13059029@gmail.com>
2021-02-11 18:11:35 +0900

    テストコードの作成

--- spec/factories/listings.rb
+++ spec/factories/listings.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :listing do
-    
+    product      {Faker::Food.dish}
+    text         {Faker::Food.dish}
+    category_id  {"3"}
+    state_id     {"3"}
+    burden_id    {"3"}
+    area_id      {"3"}
+    day_id       {"3"}
+    price        {Faker::Number.non_zero_digit}
+
+    after(:build) do |listing|
+      listing.image.attach(io: File.open('public/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Listing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  before do
+    @user = FactoryBot.create(:user)
+    @listing = FactoryBot.build(:listing, @user.id)
+  end
+
+
+describe '商品出品' do
+  context '商品出品がうまくいく場合' do
+    it "product,text,category_id,state_id,burden_id,area_id,day_id,priceが存在すれば商品出品できる" do
+      expect(@listing).to be_valid
+    end
+  end
+
+  context '商品出品がうまく行かない場合'do
+    it "ログインしていないと商品出品ができない" do
+    end
+    it "puroductが空だと商品出品できない" do
+      @listing.product = nil
+      @listing.valid?
+      expect(@listing.errors.full_messages).to include("Product can't be blank")
+    end
+    it "textが空だと商品出品できない" do
+      @listing.text = nil
+      @listing.valid?
+      expect(@listing.errors.full_messages).to include("Text can't be blank")
+    end
+    it "category_idが空だと商品出品できない"do
+      @listing.category_id = nil
+      @listing.valid?
+      expect(@listing.errors.full_messages).to include("Category can't be blank")
+    end
+    it "state_idが空だと商品出品できない" do
+      @listing.state_id = nil
+      @listing.valid?
+      expect(@listing.errors.full_messages).to include("State can't be blank")
+    end
+    it "burden_idが空だと商品出品できない" do
+      @listing.burden_id = nil
+      @listing.valid?
+      expect(@listing.errors.full_messages).to include("Burden can't be blank")
+    end
+    it "area_idが空だと商品出品できない" do
+      @listing.area_id = nil
+      @listing.valid?
+      expect(@listing.errors.full_messages).to include("Area can't be blank")
+    end
+    it "day_idが空だと商品出品できない" do
+      @listing.day_id = nil
+      @listing.valid?
+      expect(@listing.errors.full_messages).to include("Day can't be blank")
+    end
+    it "priceが空だと商品出品できない" do
+      @listing.price = nil
+      @listing.valid?
+      expect(@listing.errors.full_messages).to include("Price can't be blank")
+    end
+  end
+end
+
 end
