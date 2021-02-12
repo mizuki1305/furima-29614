bf2f5919132cde107944180f0129db2b992c0cf3
Hiroki Seike <sakaki.mizuki.13059029@gmail.com>
2021-02-12 18:18:10 +0900

    商品出品機能の実装（修正1）

--- app/javascript/packs/listing_price.js
+++ app/javascript/packs/listing_price.js
@@ -1,2 +1,11 @@
 window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const tax_price = document.getElementById("add-tax-price");
+    const addTaxDom = document.getElementById("profit");
+    const value_result = inputValue * 0.1
+    tax_price.innerHTML = (Math.floor(value_result));
+    addTaxDom.innerHTML = (Math.floor(inputValue - value_result));
+  })
 });
