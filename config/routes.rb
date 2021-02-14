ca930146179a06386b4c3352d03f60d3daf7bc11
Hiroki Seike <sakaki.mizuki.13059029@gmail.com>
2021-02-14 17:21:40 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'listings#index'
-<<<<<<< Updated upstream
-  resources :listings, only: [:index, :new, :create, :show] do
-=======
-  resources :listings, only: [:index, :new, :create :edit] do
->>>>>>> Stashed changes
+
+  resources :listings, only: [:index, :new, :create, :show, :edit] do
   end
 end
