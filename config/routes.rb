b120007e2d9e0a974833058e50a68f7f356152a0
Hiroki Seike <sakaki.mizuki.13059029@gmail.com>
2021-02-14 16:50:40 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'listings#index'
+<<<<<<< Updated upstream
   resources :listings, only: [:index, :new, :create, :show] do
+=======
+  resources :listings, only: [:index, :new, :create :edit] do
+>>>>>>> Stashed changes
   end
 end
