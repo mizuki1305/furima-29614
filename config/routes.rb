2d659ea709d7fb2f4e4d54257ead3b2909a0a011
Hiroki Seike <sakaki.mizuki.13059029@gmail.com>
2021-02-15 12:30:36 +0900

    Revert "Revert "編集機能の実装""
    This reverts commit 6daa9b0b62aacd4912b4d3c72c825e64c4193b41.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'listings#index'
+<<<<<<< Updated upstream
 
   resources :listings, only: [:index, :new, :create, :show, :edit] do
+=======
+  resources :listings, only: [:index, :new, :create, :show, :edit, :update] do
+>>>>>>> Stashed changes
   end
 end
