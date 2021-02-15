196cad08c744e6b432b927d7d62b9ff0a9879793
Hiroki Seike <sakaki.mizuki.13059029@gmail.com>
2021-02-15 21:59:54 +0900

    commit

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,8 +1,10 @@
 class ListingsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_listing, only: [:show, :edit, :update]
+    before_action :set_list, only: [:edit, :update]
 
   def index
-    @listings = Listing.all
+    @listings = Listing.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -17,10 +19,20 @@ class ListingsController < ApplicationController
       render :new
     end
   end
-
+  
   private
   def listing_params
     params.require(:listing).permit(:product, :text, :category_id, :state_id, :burden_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
+  
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def set_list
+    unless user_signed_in?
+      redirect_to action: :new
+    end
   end
 
 end
